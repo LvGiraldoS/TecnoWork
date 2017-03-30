53b0c281034b0fb30c4612ab5b538631d38a0459
adriana bogota oavlle <abogota6@misena.edu.co>
2017-03-30 13:40:42 -0500

    30/03/2017
    Elimar perfil egresado1 (Fue la primera prueba)
    Eliminar overflow, menusborrar y prueba (Son pruebas y ejemplos del
    profesor fabian) (ya estan agregados al mos mockups)
    agrega: el catalogo general con titulos y servicios falta descripcion de
    cada servicio
    agrega hoja de estilos con modificacion del menu (tamaño)

--- MockupsCSS/CatalogoGeneral.php
+++ MockupsCSS/CatalogoGeneral.php
@@ -15,221 +15,62 @@
   <tr>
     <td>
       <ul id="MenuBar1" class="MenuBarVertical">
-      <span class="texTitulos">Titulos</span>
-<li><a class="MenuBarItemSubmenu" href="#">Técnico en programación de software</a>
+      <span class="texTitulos">  ______Titulos________ </span>
+      <li><a class="MenuBarItemSubmenu" href="#">Analisis y diseño de sistemas de informació</a>
           <ul>
-          	<span class="texTitulos">Servicios que puede realizar</span>
-            <li><a href="#">Elemento 1.1</a></li>
-            <li><a href="#">Elemento 1.2</a></li>
-            <li><a href="#">Elemento 1.3</a></li>
+          	<span class="texTitulos2">___Servicios______</span>
+            <li><a href="#">Evaluaci&oacute;n de Viabilidad de proyectos empresariales</a></li>
+            <li><a href="#">Documentar c&oacute;digo y analizar las operaciones de los sistemas </a></li>
+            <li><a href="#">Soporte a operaciones de producci&oacute;n</a></li>
+            <li><a href="#">Creaci&oacute;n de Software</a></li>
+            <li><a href="#">Dise&ntilde;o de archivos y bases de datos</a></li>
+            <li><a href="#">Actualizaci&oacute;n de p&aacute;ginas web</a></li>
           </ul>
         </li>
-        <li><a href="#">Técnico en sistemas </a></li>
+        <li><a href="#" class="MenuBarItemSubmenu">Tecnico en programaci&oacute;n de software</a>
+          <ul>
+          	<span class="texTitulos2">  ___Servicios______</span>
+            <li><a href="#">Creaci&oacute;n de software </a></li>
+</ul>
+        </li>
+        <li><a class="MenuBarItemSubmenu" href="#">Tecnico en sistemas</a>
+          <ul>
+          	<span class="texTitulos2">___Servicios______</span>
+            <li><a href="#">Mantenimiento de hardware</a>            </li>
+            <li><a href="#">Reparaci&oacute;n de equipos</a></li>
+            <li><a href="#">Supervisi&oacute;n de servidores</a></li>
+            <li><a href="#">Creaci&oacute;n de scripts</a></li>
+          </ul>
+        </li>
         <li><a class="MenuBarItemSubmenu" href="#">Tecnólogo en ensamble</a>
           <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-              	<span class="texTitulos">Descripcion del servicio</span>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
+          	<span class="texTitulos2">___Servicios______</span>
+            <li><a href="#">Ensamble de equipos de computo</a>            </li>
+            <li><a href="#">Mantenimiento de equipos de informaci&oacute;n </a></li>
+</ul>
         </li>
         <li><a class="MenuBarItemSubmenu" href="#">Tecnólogo en gestion de redes </a>
           <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
+          	<span class="texTitulos2">___Servicios______</span>
+            <li><a href="#">Administrar e instalar Redes</a>            </li>
+            <li><a href="#">Mantenimiento de Redes</a></li>
+            <li><a href="#">Configurar dispositivos de red</a></li>
           </ul>
         </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Tecnólogo en telecomunicaciones </a>
+        <li><a class="MenuBarItemSubmenu" href="#">Tecnólogo en producción de multimedia </a>
           <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
+            <span class="texTitulos2">___Servicios______</span>
+            <li><a href="#">Construcci&oacute;n de contenido multimedia</a></li>
+            <li><a href="#">Producci&oacute;n de Publicidad</a></li>
           </ul>
         </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Tecnólogo en diseño de integracion de multimedia</a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Tecnólogo en Telecomunicaciones. </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Tecnología en Diseño, Implementación y Mantenimiento de Sistemas de Telecomunicaciones </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Tecnólogo en la implementación de Servicios de Telecomunicaciones </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
-        <li><a class="MenuBarItemSubmenu" href="#">Técnico en instalaciones electricas en baja tensión </a>
-          <ul>
-            <li><a class="MenuBarItemSubmenu" href="#">Elemento 3.1</a>
-              <ul>
-                <li><a href="#">Elemento 3.1.1</a></li>
-                <li><a href="#">Elemento 3.1.2</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Elemento 3.2</a></li>
-            <li><a href="#">Elemento 3.3</a></li>
-          </ul>
-        </li>
+        <li><a href="#">Tecn&oacute;logo en Telecomunicaciones</a></li>
+        <li><a href="#">Tecn&oacute;logo en Dise&ntilde;o, Implementaci&oacute;n y Mantenimiento de sistemas de telecomunicaciones</a></li>
+        <li><a href="#">Tecn&oacute;logo en la implementaci&oacute;n de Servicios de Telecomunicaciones</a></li>
+        <li><a href="#">Tecn&oacute;logo en mantenimiento de equipos de c&oacute;mputo diseño e instalación de cableado estructurado</a></li>
+        <li><a href="#">Tecn&oacute;logo en Administraci&oacute;n de Redes de Computadores</a></li>
+        <li><a href="#">Tecn&oacute;logo en Implementaci&oacute;n de Servicios de Telecomunicaciones por redes cableadas</a></li>
+        <li><a href="#">Tecnólogo en telecomunicaciones residenciales y corporativas </a></li>
       </ul>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
