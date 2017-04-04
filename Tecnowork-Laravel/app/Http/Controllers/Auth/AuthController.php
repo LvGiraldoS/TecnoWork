8d93dd1c2752f169b2ee69d1d339050439425e62
LvGiraldoS <lvgiraldo32@misena.edu.co>
2017-04-04 18:01:27 -0500

    04/Abril/2017
    Error en el AuthController por falla en actualización.

--- Tecnowork-Laravel/app/Http/Controllers/Auth/AuthController.php
+++ Tecnowork-Laravel/app/Http/Controllers/Auth/AuthController.php
@@ -1,22 +1,10 @@
 <?php
-/**
-* @file AuthController.php
-* @Author LV - Andres Otalora
-* @date 
-* @brief implementación del controlador automatico para login
-*/
 namespace App\Http\Controllers\Auth;
-
 use App\User;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
-
-/**
-* @class AuthController
-* @brief Representa una clase automatica para login
-*/
 class AuthController extends Controller
 {
     /*
@@ -29,28 +17,22 @@ class AuthController extends Controller
     | a simple trait to add these behaviors. Why don't you explore it?
     |
     */
-
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-
     /**
      * Create a new authentication controller instance.
      *
      * @return void
      */
-
-    
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'getLogout']);
     }
-
     /**
      * Get a validator for an incoming registration request.
      *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -59,7 +41,6 @@ class AuthController extends Controller
             'password' => 'required|confirmed|min:6',
         ]);
     }
-
     /**
      * Create a new user instance after a valid registration.
      *
